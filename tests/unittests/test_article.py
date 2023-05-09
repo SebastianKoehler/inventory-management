e9deca17422edd7b0e55e5a69e119279607b6787
Invisus666 <Developer-Sebastian.Koehler@gmx.de>
2023-05-09 18:51:14 +0200

    Commit to push actual state to branch.
    - Updated test_article.py
       - Created test for the constructor
       - Created test for "tell_if_product_is_expired"

--- tests/unittests/test_article.py
+++ tests/unittests/test_article.py
@@ -8,6 +8,20 @@ class TestArticle:
     article_bbd_date = datetime.date(2023, 5, 22)
     article = Article("Bohnen", 10, article_bbd_date, "Keller")
 
+    # Testing constructor
+    def test_init(self):
+        holds_correct_class = isinstance(self.article, Article)
+        has_name = hasattr(self.article, "article_name")
+        has_amount = hasattr(self.article, "article_amount")
+        has_bbd = hasattr(self.article, "article_bbd")
+        has_location = hasattr(self.article, "article_storage_location")
+
+        assert holds_correct_class is True
+        assert has_name is True
+        assert has_amount is True
+        assert has_bbd is True
+        assert has_location is True
+
     # Testing getter functions
     def test_get_article_name(self):
         assert "Bohnen" == self.article.article_name
@@ -45,3 +59,41 @@ class TestArticle:
         new_article_storage_location = "Wohnung"
         self.article.article_storage_location = new_article_storage_location
         assert new_article_storage_location == self.article.article_storage_location
+
+    # Testing class functions
+    def test_tell_if_product_is_expired(self):
+
+        actual_date = datetime.date(2023, 5, 22)
+
+        expiration_date_today = datetime.date(2023, 5, 22)
+        expiration_date_time_over = datetime.date(2023, 2, 10)
+        expiration_date_time_left = datetime.date(2024, 5, 17)
+
+        self.article_dummy_one = Article("Joghurt", 3, expiration_date_today, "Vorratsschrank")
+        self.article_dummy_two = Article("Schinken", 1, expiration_date_time_over, "Kühlschrank")
+        self.article_dummy_three = Article("Getrocknete Tomaten", 5, expiration_date_time_left, "Vorratsschrank")
+
+        difference_days_article_dummy_one = actual_date - self.article_dummy_one.article_bbd
+        difference_days_article_dummy_two = actual_date - self.article_dummy_two.article_bbd
+        difference_days_article_dummy_three = actual_date - self.article_dummy_three.article_bbd
+
+        difference_days_equals_zero = difference_days_article_dummy_one.days
+        difference_days_positive_number = difference_days_article_dummy_two.days
+        difference_days_negative_number = difference_days_article_dummy_three.days
+
+        def test_tell_if_product_is_expired(difference_days):
+
+            if difference_days == 0:
+                return f"The product expired today."
+            elif difference_days > 0:
+                return f"The product has expired for {difference_days} days."
+            else:
+                return f"The product has a shelf life of {abs(difference_days)} days left."
+
+        result_one = test_tell_if_product_is_expired(difference_days_equals_zero)
+        result_two = test_tell_if_product_is_expired(difference_days_positive_number)
+        result_three = test_tell_if_product_is_expired(difference_days_negative_number)
+
+        assert f"The product expired today." in result_one
+        assert f"The product has expired for {difference_days_positive_number} days." in result_two
+        assert f"The product has a shelf life of {abs(difference_days_negative_number)} days left." in result_three
